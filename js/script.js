0c139dcb39170a2d6b09b926f9bedbc4b95a570b
nitncwind <42908899+nitncwind@users.noreply.github.com>
2018-09-02 22:16:44 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,63 +1,65 @@
-// menu bar
-	if( 'ontouchstart' in window ){ var click = 'touchstart'; }
-	else { var click = 'click'; }
+// http://photoshopvip.net/97481 Menu 1
 
+if ("ontouchstart" in window) {
+  	var click = "touchstart";
+} else {
+  	var click = "click";
+}
 
-	$('div.menu-burger').on(click, function(){
-
-			if( !$(this).hasClass('open') ){ openMenu(); } 
-			else { closeMenu(); }
-
-	});
-	
-
-	$('div.menu ul li a').on(click, function(e){
-		e.preventDefault();
+$("div.burger").on(click, function() {
+	if (!$(this).hasClass("open")) {
+		openMenu();
+	} else {
 		closeMenu();
-	});		
-
-
-	function openMenu(){
-		
-		$('div.circle').addClass('expand');
-					
-		$('div.menu-burger').addClass('open');	
-		$('div.x, div.y, div.z').addClass('menu-collapse');
-		$('.menu li').addClass('animate');
-		
-		setTimeout(function(){ 
-			$('div.y').hide(); 
-			$('div.x').addClass('rotate30'); 
-			$('div.z').addClass('rotate150'); 
-		}, 70);
-		setTimeout(function(){
-			$('div.x').addClass('rotate45'); 
-			$('div.z').addClass('rotate135');  
-		}, 120);
-		
-		
-
 	}
-	
-	function closeMenu(){
+});
 
-		$('div.menu-burger').removeClass('open');	
-		$('div.x').removeClass('rotate45').addClass('rotate30'); 
-		$('div.z').removeClass('rotate135').addClass('rotate150');				
-		$('div.circle').removeClass('expand');
-		$('.menu li').removeClass('animate');
-		
-		setTimeout(function(){ 			
-			$('div.x').removeClass('rotate30'); 
-			$('div.z').removeClass('rotate150'); 			
-		}, 50);
-		setTimeout(function(){
-			$('div.y').show(); 
-			$('div.x, div.y, div.z').removeClass('menu-collapse');
-		}, 70);													
-		
-	}
+$("div.menu ul li a").on(click, function(e) {
+	e.preventDefault();
+	closeMenu();
+});
 
-$(".link").click(function () {
-	location.href = $(this).attr("href");
+function openMenu() {
+	$("div.circle").addClass("expand");
+
+	$("div.burger").addClass("open");
+	$("div.x, div.y, div.z").addClass("collapse");
+	$(".menu li").addClass("animate");
+
+	setTimeout(function() {
+		$("div.y").hide();
+		$("div.x").addClass("rotate30");
+		$("div.z").addClass("rotate150");
+	}, 100);
+	setTimeout(function() {
+		$("div.x").addClass("rotate45");
+		$("div.z").addClass("rotate135");
+	}, 180);
+}
+
+function closeMenu() {
+	$("div.burger").removeClass("open");
+	$("div.x")
+		.removeClass("rotate45")
+		.addClass("rotate30");
+	$("div.z")
+		.removeClass("rotate135")
+		.addClass("rotate150");
+	$("div.circle").removeClass("expand");
+	$(".menu li").removeClass("animate");
+
+	setTimeout(function() {
+		$("div.x").removeClass("rotate30");
+		$("div.z").removeClass("rotate150");
+	}, 50);
+	setTimeout(function() {
+		$("div.y").show();
+		$("div.x, div.y, div.z").removeClass("collapse");
+	}, 70);
+}
+
+$(".link").click(function() {
+	console.log("hogehgoehgeo");
+	  // windows.location.href = $(this).attr("href");
+	window.location.href = "http://127.0.0.1:5500/base.html";
 });
